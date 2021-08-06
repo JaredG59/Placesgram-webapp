aa96b19e7c46462d327ad36ead9d761faad8ad86
JaredG <65161768+JaredG59@users.noreply.github.com>
2021-08-06 01:56:23 -0700

    started routes and implementation of api

--- FRONTEND/src/App.js
+++ FRONTEND/src/App.js
@@ -57,7 +57,7 @@ const App = () => {
         <Route path="/auth">
             <Auth />
           </Route>
-          <Redirect to="/auth" />
+          <Redirect to="/" />
         </React.Fragment>
     );
   }
